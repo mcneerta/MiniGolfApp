6b70fa96d8836316e0c28f22c685929ca4853d94
Divya Shrivastava <div.shrivastava21@gmail.com>
2021-03-24 19:13:52 -0400

    Created a list with player name and their respective total scores displayed after game finished

--- app/src/main/java/com/example/minigolfapp/Results.java
+++ app/src/main/java/com/example/minigolfapp/Results.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import static android.view.Gravity.CENTER;
+import static android.view.Gravity.CENTER_HORIZONTAL;
+import static android.view.Gravity.CENTER_VERTICAL;
 
 public class Results extends AppCompatActivity {
 
@@ -19,8 +25,30 @@ public class Results extends AppCompatActivity {
 
     public void init(){
         TextView winner = (TextView) findViewById(R.id.winner);
+        TableLayout playerNameList = (TableLayout) findViewById(R.id.playerNamesList);
+        TableLayout playerScoreList = (TableLayout) findViewById(R.id.playerScoresList);
         String winnerMessage = "The winner is " + Scorecard.winnerName + "!";
         winner.setText(winnerMessage);
+
+        for (int i = 0; i < Scorecard.nameList.size(); i++) {
+            TableRow nRow = new TableRow(this);
+            TextView name = new TextView(this);
+            name.setHeight(136);
+            name.setText(Scorecard.nameList.get(i).getText().toString());
+            name.setTextSize(18);
+//            name.setGravity(CENTER);
+            nRow.addView(name);
+            playerNameList.addView(nRow);
+
+            TableRow sRow = new TableRow(this);
+            TextView playerTotal = new TextView(this);
+            playerTotal.setHeight(136);
+            playerTotal.setText(Scorecard.totalList.get(i).getText().toString());
+            playerTotal.setTextSize(18);
+//            playerTotal.setGravity(CENTER_VERTICAL);
+            sRow.addView(playerTotal);
+            playerScoreList.addView(sRow);
+        }
     }
 
     public void returnClick(View v) {
