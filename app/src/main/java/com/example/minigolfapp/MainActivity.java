de68e03b29e09ca6536971b0ef21df6ace50bb17
Austen Gralheer <icefanf35@gmail.com>
2021-03-29 13:16:33 -0400

    Extra toast for no entry

--- app/src/main/java/com/example/minigolfapp/MainActivity.java
+++ app/src/main/java/com/example/minigolfapp/MainActivity.java
@@ -64,6 +64,13 @@ public class MainActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(context, text, duration);
                         toast.show();
                     }
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "You need at least one player!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
             }
         });
