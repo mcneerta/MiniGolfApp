d6efa035014ee5dbf0575666bd256edb71be6f1c
Divya Shrivastava <div.shrivastava21@gmail.com>
2021-04-12 12:01:51 -0400

    Help page returns to scorecard; does not keep updated info yet

--- app/src/main/java/com/cspals/minigolfapp/Help.java
+++ app/src/main/java/com/cspals/minigolfapp/Help.java
@@ -2,7 +2,9 @@ package com.cspals.minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Help extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void returnClick(View v) {
+        startActivity(new Intent(Help.this, Scorecard.class));
     }
 }
