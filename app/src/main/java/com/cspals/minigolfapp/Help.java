4c797c440a844d7d5e9e9f200c54d845690f6b5d
Divya Shrivastava <div.shrivastava21@gmail.com>
2021-04-21 11:56:17 -0400

    Began adding text to Help page

--- app/src/main/java/com/cspals/minigolfapp/Help.java
+++ app/src/main/java/com/cspals/minigolfapp/Help.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Help extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        TextView helpText = findViewById(R.id.textHelp);
+        helpText.setText("1. Please select each box to input a score\n\n2. The column indicates the hole number, while the row indicates the players. The total is displayed to the right.\n\nTo include par, click on the gear icon and click on 'Change Scorecard'");
     }
 
     public void returnClick(View v) {
