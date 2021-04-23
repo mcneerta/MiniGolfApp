cdcf50d573e4549022410fded95ac9c31ae4c917
delinskijc <delinskijc@vcu.edu>
2021-04-23 02:56:55 -0400

    Added change scorecard screen

---
+++ app/src/main/java/com/cspals/minigolfapp/ChangeScorecard.java
@@ -0,0 +1,15 @@
+package com.cspals.minigolfapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeScorecard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changescorecard);
+
+    }
+}
