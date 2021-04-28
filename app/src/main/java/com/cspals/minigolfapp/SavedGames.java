6f17061e6146403812d9a190c41a1d8b4495839d
Austen Gralheer <icefanf35@gmail.com>
2021-04-28 15:33:54 -0400

    Continued work on displaying saved games

--- app/src/main/java/com/cspals/minigolfapp/SavedGames.java
+++ app/src/main/java/com/cspals/minigolfapp/SavedGames.java
@@ -2,11 +2,15 @@ package com.cspals.minigolfapp;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SavedGames extends AppCompatActivity {
+
+    public Button[] buttons = {findViewById(R.id.SavedGame1), findViewById(R.id.SavedGame2), findViewById(R.id.SavedGame3), findViewById(R.id.SavedGame4), findViewById(R.id.SavedGame5)};
+    public GameSave[] saves = MainActivity.games;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
